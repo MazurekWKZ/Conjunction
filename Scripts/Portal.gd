aaa0d546c880ce42c1bd75f975b1113f7910d4f3
unknown <mazur.l@10g.pl>
2024-03-02 17:17:34 +0100

    portals sorted out

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -3,13 +3,11 @@ class_name Portal extends Node2D
 @onready var portal_area = $Area2D
 @export var target_node = Node2D
 
-
 var state = "free"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,13 +16,15 @@ func _process(delta):
 		var detected_areas = portal_area.get_overlapping_areas()
 		var parent_object = detected_areas[0].get_parent()
 		if target_node.state == "free" && parent_object is Node2D:
+			target_node.state = "wait"
 			parent_object.is_moving = false
 			parent_object.target_position = target_node.global_position
-			parent_object.global_position = target_node.global_position
-			
+			parent_object.global_position = target_node.global_position		
 		print(state)
 	
 	if !portal_area.has_overlapping_areas() && state == "busy":
 		state = "free"
 		print(state)
-	pass
+	
+	if portal_area.has_overlapping_areas() && state == "wait":
+		state = "busy"	
