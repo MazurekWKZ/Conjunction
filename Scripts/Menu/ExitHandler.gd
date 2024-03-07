3da284dcca96f13930325030793cea2dcbf93c27
unknown <mazur.l@10g.pl>
2024-03-08 00:04:04 +0100

    music added

--- Scripts/Menu/ExitHandler.gd
+++ Scripts/Menu/ExitHandler.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 @onready var main_menu_path = "res://Levels/MainMenu.tscn"
+@onready var audio_stream = $"../Background_music"
 
 var retry_timeout = 1.0
 var retry_timer = 0.0
 var timer_enabled = false
+var target_volume_db = 0.0
+var volume_decrease_speed = 0.02
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,15 +21,21 @@ func _process(delta):
 			print("time exceeded")
 			var current_scene_path = get_tree().current_scene.scene_file_path
 			get_tree().change_scene_to_file(current_scene_path)
-	
+	else:
+		retry_timer = 0
+		
+	if audio_stream != null:
+		audio_stream.volume_db = lerp(audio_stream.volume_db, target_volume_db, volume_decrease_speed)
 
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_R:
 			timer_enabled = true
-		else:
+			target_volume_db = -30.0
+	if event is InputEventKey and event.is_released():
+		if event.keycode == KEY_R:
 			timer_enabled = false
-			retry_timer = 0.0
+			target_volume_db = 0.0
 			
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().change_scene_to_file(main_menu_path)
