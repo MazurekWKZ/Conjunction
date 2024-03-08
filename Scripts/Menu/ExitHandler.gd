39890dc9b565dc4b369b21b83e584bcc55cbe075
unknown <mazur.l@10g.pl>
2024-03-08 23:12:47 +0100

    added fade in/out effects to menu and level

--- Scripts/Menu/ExitHandler.gd
+++ Scripts/Menu/ExitHandler.gd
@@ -2,13 +2,17 @@ extends Node2D
 
 @onready var main_menu_path = "res://Levels/MainMenu.tscn"
 @onready var audio_stream = $"../Background_music"
+@onready var fade_effect = $"../EffectLayer/".get_child(0)
 
 var retry_timeout = 1.0
 var retry_timer = 0.0
 var timer_enabled = false
 var target_volume_db = 0.0
 var volume_decrease_speed = 0.02
-
+var fade_speed = 0.018
+var target_fade = 0.0
+var fade = 1.0
+var exit_offset = 0.1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -26,16 +30,27 @@ func _process(delta):
 		
 	if audio_stream != null:
 		audio_stream.volume_db = lerp(audio_stream.volume_db, target_volume_db, volume_decrease_speed)
+	
+	if target_fade >= 1.0:
+		fade = clamp(fade + target_fade * fade_speed, 0.0, 2.0)
+	else:
+		fade = clamp(fade - fade_speed, 0.0, 2.0)
+	fade_effect.material.set_shader_parameter("luma_threshold", fade)
+	
+	if fade > 1 + exit_offset:
+		get_tree().change_scene_to_file(main_menu_path)
+	
 
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_R:
 			timer_enabled = true
-			target_volume_db = -30.0
+			target_volume_db = -40.0
 	if event is InputEventKey and event.is_released():
 		if event.keycode == KEY_R:
 			timer_enabled = false
 			target_volume_db = 0.0
 			
 	if event.is_action_pressed("ui_cancel"):
-		get_tree().change_scene_to_file(main_menu_path)
+		target_volume_db = -40.0
+		target_fade = 1.0	
--- Scripts/Menu/Menu_handler.gd
+++ Scripts/Menu/Menu_handler.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var begin_button = $Begin
 @onready var level_button = $Level
+@onready var fade_effect = $"../EffectLayer".get_child(0)
 
 @export var modulation_speed = 0.15
 @export var inactive_alpha = 0.2
@@ -10,6 +11,11 @@ var next_level_path = "res://Levels/TestLevel.tscn"
 var state = "main_menu"
 var begin_button_state = true
 var level_button_state = false
+var fade_speed = 0.018
+var target_fade = 0.0
+var fade = 1.0
+var exit_offset = 0.1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -23,7 +29,8 @@ func _input(event):
 			level_button_state = !level_button_state
 		if event.is_action_pressed("ui_accept"):
 			if begin_button_state:
-				get_tree().change_scene_to_file(next_level_path)
+				state = "locked"
+				target_fade = 1.0
 			elif level_button_state:
 				pass
 			
@@ -41,4 +48,13 @@ func _process(delta):
 		level_button_alpha = inactive_alpha
 	level_button.modulate.a = lerp(level_button.modulate.a, level_button_alpha, modulation_speed)
 	begin_button.modulate.a = lerp(begin_button.modulate.a, begin_button_alpha, modulation_speed)
+	
+	if target_fade >= 1.0:
+		fade = clamp(fade + target_fade * fade_speed, 0.0, 2.0)
+	else:
+		fade = clamp(fade - fade_speed, 0.0, 2.0)
+	fade_effect.material.set_shader_parameter("luma_threshold", fade)
+	
+	if fade > 1 + exit_offset:
+		get_tree().change_scene_to_file(next_level_path)
 	pass
